8247e4cf0a6b198f05f5485bea07f2579aa28038
esdhanush <esdhanush@gmail.com>
2023-08-21 11:43:47 +0530

    Updated both files and deleted a.html and b.py

---
+++ dayskill/b.py
@@ -0,0 +1,8 @@
+# Sample Python Code
+def greet(name):
+    """This function greets the person passed in as a parameter."""
+    print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    greet(user_name)
